15aeb2cade808dd7cefcab86f2c8d6b29d5add9e
DESKTOP-QLS9EQQ\Nutzer <d>
2025-02-07 17:40:36 +0100

    fix drowning and add vertical friction

--- src/main/kotlin/com/github/bonndan/blimpy/setup/ModSounds.kt
+++ src/main/kotlin/com/github/bonndan/blimpy/setup/ModSounds.kt
@@ -1,43 +1,26 @@
 package com.github.bonndan.blimpy.setup
 
 import com.github.bonndan.blimpy.BlimpyMod
+import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.sounds.SoundEvent
+import net.neoforged.bus.api.IEventBus
+import net.neoforged.neoforge.registries.DeferredRegister
+import net.neoforged.neoforge.registries.DeferredRegister.create
 import java.util.function.Supplier
 
 object ModSounds {
-    
-    val STEAM_WHISTLE: Supplier<SoundEvent> = Registration.SOUND_EVENTS.register("steam_tug_whistle",
+
+    private val SOUND_EVENTS: DeferredRegister<SoundEvent> = create(BuiltInRegistries.SOUND_EVENT, BlimpyMod.MOD_ID)
+
+    val HISS: Supplier<SoundEvent> = SOUND_EVENTS.register(
+        "hiss",
         Supplier {
-            SoundEvent.createFixedRangeEvent(
-                ResourceLocation.fromNamespaceAndPath(
-                    BlimpyMod.MOD_ID,
-                    "steam_tug_whistle"
-                ), 64f
-            )
+            SoundEvent.createFixedRangeEvent(ResourceLocation.fromNamespaceAndPath(BlimpyMod.MOD_ID, "hiss"), 64F)
         })
 
-    
-    val DOCKING: Supplier<SoundEvent> = Registration.SOUND_EVENTS.register("tug_docking",
-        Supplier {
-            SoundEvent.createFixedRangeEvent(
-                ResourceLocation.fromNamespaceAndPath(
-                    BlimpyMod.MOD_ID,
-                    "tug_docking"
-                ), 64f
-            )
-        })
+    fun register(eventBus: IEventBus) {
+        SOUND_EVENTS.register(eventBus)
+    }
 
-    
-    val UNDOCKING: Supplier<SoundEvent> = Registration.SOUND_EVENTS.register("tug_undocking",
-        Supplier {
-            SoundEvent.createFixedRangeEvent(
-                ResourceLocation.fromNamespaceAndPath(
-                    BlimpyMod.MOD_ID,
-                    "tug_undocking"
-                ), 64f
-            )
-        })
-
-    fun register() {}
 }
