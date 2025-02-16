3e4225473e4b64408ad6e7ac1e79b08b712ca5d7
DESKTOP-QLS9EQQ\Nutzer <d>
2025-02-16 13:02:57 +0100

    quick move fix

--- src/main/kotlin/com/github/bonndan/blimpy/blimp/container/BlimpMenu.kt
+++ src/main/kotlin/com/github/bonndan/blimpy/blimp/container/BlimpMenu.kt
@@ -77,28 +77,35 @@ class BlimpMenu(
         val sourceStack = sourceSlot.item
         val copyOfSourceStack = sourceStack.copy()
 
-        // Check if the slot clicked is one of the vanilla container slots
-        if (index < VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT) {
-            // This is a vanilla container slot so merge the stack into the tile inventory
-            if (!moveItemStackTo(
-                    sourceStack, TE_INVENTORY_FIRST_SLOT_INDEX, TE_INVENTORY_FIRST_SLOT_INDEX + BlimpEntity.CONTAINER_SIZE, false
-                )
-            ) {
-                return ItemStack.EMPTY // EMPTY_ITEM
+        when {
+
+            // move from player to entity
+            index < VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT -> {
+
+                if (!moveItemStackTo(
+                        sourceStack,
+                        TE_INVENTORY_FIRST_SLOT_INDEX,
+                        TE_INVENTORY_FIRST_SLOT_INDEX + BlimpEntity.CONTAINER_SIZE + 1,
+                        false
+                    )
+                ) {
+                    return ItemStack.EMPTY
+                }
             }
-        } else if (index < TE_INVENTORY_FIRST_SLOT_INDEX + BlimpEntity.CONTAINER_SIZE) {
-            // This is a TE slot so merge the stack into the players inventory
-            if (!moveItemStackTo(
-                    sourceStack,
-                    VANILLA_FIRST_SLOT_INDEX,
-                    VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT,
-                    false
-                )
-            ) {
-                return ItemStack.EMPTY
+            index <= TE_INVENTORY_FIRST_SLOT_INDEX + BlimpEntity.CONTAINER_SIZE -> {
+                // This is a TE slot so merge the stack into the players inventory
+                if (!moveItemStackTo(
+                        sourceStack,
+                        VANILLA_FIRST_SLOT_INDEX,
+                        VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT,
+                        false
+                    )
+                ) {
+                    return ItemStack.EMPTY
+                }
             }
-        } else {
-            return ItemStack.EMPTY
+
+            else -> return ItemStack.EMPTY
         }
         // If stack size == 0 (the entire stack was moved) set slot contents to null
         if (sourceStack.count == 0) {
